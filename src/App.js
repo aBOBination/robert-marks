37c8d5e432fb12aeacd429e57b9108bf668d8373
Robert Marks <rmarks@nav.com>
2020-07-14 16:43:33 -0600

    sync

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
